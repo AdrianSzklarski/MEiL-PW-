b6512a17b82d341b944614eeebfb8b8d142a1448
AdrianSzklarski <adrian_it@o2.pl>
2024-11-05 09:56:16 +0100

    Added file

--- Język C/Informatyka 1/Zajecia 3/ogyginal/p9.6.zad.c
+++
@@ -1,2 +0,0 @@
-// Napisac program liczacy objętośc prostopadloscianu i ostrslupa o takiej samej podstawie a oraz b, wyskosci h.
-// Paramtery a, b, h należy podać z klawiatury, a wyniki obliczeń wyświetlić na ekranie
